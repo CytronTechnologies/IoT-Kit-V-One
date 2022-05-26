b86c3068c2cb0faab3e7bb2229e53cdb73d8e482
NorHairil <79236067+NorHairil@users.noreply.github.com>
2022-05-26 12:30:57 +0800

    Update vonesetting.h

--- NodeMCU-32S/Libraries/VOneMqttClient/vonesetting.h
+++ NodeMCU-32S/Libraries/VOneMqttClient/vonesetting.h
@@ -4,14 +4,14 @@
 #error Multiple includes of vonesetting.h
 #endif
 
-#define WIFI_SSID "Cytron"
-#define WIFI_PASSWORD "f5f4f3f2f1"
+#define WIFI_SSID "Your SSID"
+#define WIFI_PASSWORD "Your Password"
 
 #define MQTT_SERVER "mqtt.v-one.my"
 #define MQTT_PORT 8883
 #define MQTT_USERNAME "4g44tnIHjBnYQPxz"
 #define MQTT_PASSWORD " "
 
-#define GATEWAYID "67c4a3c1-3b94-4df7-a253-8912eab256b0"
+#define GATEWAYID "67c4a3c1-3b94-4df7-a253-8912eab256b1"
 #define INTERVAL 10000 //10S
 #define INTERVAL2 500 //0.5S
