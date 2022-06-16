7a7feb96d84a14c76e987d87340cc06206893cf6
NorHairil <79236067+NorHairil@users.noreply.github.com>
2022-06-16 17:54:24 +0800

    Add files via upload

---
+++ vonesetting.h
@@ -0,0 +1,17 @@
+#if !defined (VONESETTING_H)
+#define VONESETTING_H
+#else
+#error Multiple includes of vonesetting.h
+#endif
+
+#define WIFI_SSID "Cytron"
+#define WIFI_PASSWORD "f5f4f3f2f1"
+
+#define MQTT_SERVER "mqtt.v-one.my"
+#define MQTT_PORT 8883
+#define MQTT_USERNAME "49DUJmqvBK9IHF2r"
+#define MQTT_PASSWORD " "
+
+#define GATEWAYID "e466f5da-e3a0-4f79-b383-8524e0e654df"
+#define INTERVAL 1000 //1S
+#define INTERVAL2 500 //0.5S
