4d073cf4c1e0f6d023e1258c17ec178800feaeea
NorHairil <79236067+NorHairil@users.noreply.github.com>
2022-06-20 16:52:20 +0800

    Update vonesetting.h

--- vonesetting.h
+++ vonesetting.h
@@ -4,14 +4,14 @@
 #error Multiple includes of vonesetting.h
 #endif
 
-#define WIFI_SSID "Cytron"
-#define WIFI_PASSWORD "f5f4f3f2f1"
+#define WIFI_SSID "YOUR_SSID"          //Replace this with YOUR WiFi SSID
+#define WIFI_PASSWORD "YOUR_PASSWORD"  //Replace this with YOUR WiFi Password
 
 #define MQTT_SERVER "mqtt.v-one.my"
 #define MQTT_PORT 8883
-#define MQTT_USERNAME "49DUJmqvBK9IHF2r"
+#define MQTT_USERNAME "49DUJmqvBK9IHF2r"  //Replace this with the Access Token of YOUR gateway
 #define MQTT_PASSWORD " "
 
-#define GATEWAYID "e466f5da-e3a0-4f79-b383-8524e0e654df"
+#define GATEWAYID "e466f5da-e3a0-4f79-b383-8524e0e654df"  //Replace this with the gatewayID of your gateway
 #define INTERVAL 1000 //1S
 #define INTERVAL2 500 //0.5S
