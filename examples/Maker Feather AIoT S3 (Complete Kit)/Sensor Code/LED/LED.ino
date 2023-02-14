991b73b94402d180bb4f34dc47e547c35f2ff91d
NorHairil <79236067+NorHairil@users.noreply.github.com>
2023-02-14 11:20:20 +0800

    Update LED.ino

--- examples/Maker Feather AIoT S3 (Complete Kit)/Sensor Code/LED/LED.ino
+++ examples/Maker Feather AIoT S3 (Complete Kit)/Sensor Code/LED/LED.ino
@@ -5,10 +5,10 @@ ESP32 publish telemetry data to VOne Cloud and subcribe to execute controller (L
 #include "VOneMqttClient.h"
 
 //define device id
-const char* LEDLight = "4d785918-9050-4e91-8567-b4b96533d671"; //led light
+const char* LEDLight = "4d785918-9050-4e91-8567-b4b96533d671";      //Replace this with YOUR deviceID for the LED
 
 //Used Pins
-const int ledPin = 33;
+const int ledPin = 2;      //Onboard LED
 
 //Output
 
