a14897b9a886030b5f65a9b6d066bed71037167d
NorHairil <79236067+NorHairil@users.noreply.github.com>
2023-03-17 17:02:51 +0800

    Update 10. Smart_Kitchen.ino

--- examples/NodeMCU-32S/Projects/10. Smart_Kitchen/10. Smart_Kitchen.ino
+++ examples/NodeMCU-32S/Projects/10. Smart_Kitchen/10. Smart_Kitchen.ino
@@ -161,7 +161,7 @@ void loop() {
 
     //Publish telemetry data 2
     float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    int t = dht.readTemperature();
 
     JSONVar payloadObject;
     payloadObject["Humidity"] = h;
